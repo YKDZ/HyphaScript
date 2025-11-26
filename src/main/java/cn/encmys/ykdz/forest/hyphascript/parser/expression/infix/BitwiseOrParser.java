ec51224e8434ed56f65abee96eeb3fb68546ce28
YKDZ <3070799584@qq.com>
2025-11-26 12:35:47 +0800

    v0.1.2-Beta: bitwise and / or

--- src/main/java/cn/encmys/ykdz/forest/hyphascript/parser/expression/infix/BitwiseOrParser.java
+++ src/main/java/cn/encmys/ykdz/forest/hyphascript/parser/expression/infix/BitwiseOrParser.java
@@ -1,7 +1,7 @@
 package cn.encmys.ykdz.forest.hyphascript.parser.expression.infix;
 
 import cn.encmys.ykdz.forest.hyphascript.node.ASTNode;
-import cn.encmys.ykdz.forest.hyphascript.node.BitOr;
+import cn.encmys.ykdz.forest.hyphascript.node.BitwiseOr;
 import cn.encmys.ykdz.forest.hyphascript.parser.ParseContext;
 import cn.encmys.ykdz.forest.hyphascript.parser.PrecedenceTable;
 import cn.encmys.ykdz.forest.hyphascript.parser.expression.ExpressionParser;
@@ -17,6 +17,6 @@ public class BitwiseOrParser implements ExpressionParser.Infix {
     @Override
     public @NotNull ASTNode parse(@NotNull ParseContext ctx, @NotNull ASTNode left) {
         Token op = ctx.consume(Token.Type.BIT_OR);
-        return new BitOr(left, ctx.parseExpression(precedence()), op, ctx.current());
+        return new BitwiseOr(left, ctx.parseExpression(precedence()), op, ctx.current());
     }
 }
